59f54326bbf51f1f659e48c2ab4fb3b5b6434da5
Anton <trusov.anton.02@gmail.com>
2017-03-12 02:53:37 +0300

    Add files via upload

--- Objects/MovableObject.cpp
+++ Objects/MovableObject.cpp
@@ -1,4 +1,5 @@
 #include "MovableObject.hpp"
+#include <iostream> //TODO delete
 MovableObject::MovableObject()
 {
 }
@@ -12,6 +13,33 @@ void MovableObject:: moveRelative(glm :: dvec3 to)
 {
 	 coord += A * to; 
 }
+
+void MovableObject:: rotateAbslute(glm :: dvec3 omega, double dt)
+{
+	if(glm::dot(omega, omega) == 0)//если нам не нужно крутиться экономим операции
+		return;
+	glm :: dmat3 Omega(0, omega.z, -omega.y, //первый столбец
+						-omega.z, 0, omega.x, //второй
+						omega.y, -omega.x, 0);//третий. ЭТО GLM
+	A += Omega * A * dt;
+}
+
+void MovableObject:: rotateRelative(glm :: dvec3 omega, double dt)
+{
+	if(glm::dot(omega, omega) == 0)//если нам не нужно крутиться экономим операции
+		return;
+	if(!oneByOne)
+	{
+		glm :: dmat3 Omega(	0, omega.z, -omega.y, //первый столбец
+							-omega.z, 0, omega.x, //второй
+							omega.y, -omega.x, 0);//третий. ЭТО GLM
+		A += A*Omega * dt; 
+	}
+	ex /= sqrt(glm:: dot(ex, ex));
+	ey /= sqrt(glm:: dot(ey, ey));
+	ez /= sqrt(glm:: dot(ez, ez));
+}
+
 
 MovableObject::~MovableObject()
 {
--- Objects/Object3D.hpp
+++ Objects/Object3D.hpp
@@ -2,12 +2,13 @@
 #define _OBJECT3D
 #include "DrawableObject.hpp"
 #include <glm/glm.hpp>
+#include <cmath>
 
 class Object3D : public DrawableObject
 {
 protected:
-	glm :: dvec3 coord = glm :: dvec3 (-100, 0, 0);//абсолютные координаты 
-	glm :: dmat3x3 A = glm :: dmat3x3( glm :: dvec3 (1, 0, 0),  glm :: dvec3 (0, 1, 0),  glm :: dvec3 (0, 0, 1)); //направление прямо, направление налево, направление вверх
+	glm :: dvec3 coord = glm :: dvec3 (-20, 0, 0);//абсолютные координаты 
+	glm :: dmat3 A = glm :: dmat3x3( glm :: dvec3 (1, 0, 0),  glm :: dvec3 (0, 1, 0),  glm :: dvec3 (0, 0, 1)); //направление прямо, направление налево, направление вверх
 	glm :: dvec3  &ex = A[0];
 	glm :: dvec3  &ey = A[1];
 	glm :: dvec3  &ez = A[2]; 
